f9254286da74e1b4c433e6dddd6da310baaf2255
wannabefrontdeveloper <giwonk@naver.com>
2024-03-10 21:10:32 +0900

    컴포넌트 생성 MainTab 컴포넌트를 생성하였습니다.

---
+++ screens/MainTab.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+function MainTab() {
+  return <Tab.Navigator>{/* TODO: 화면 추가 */}</Tab.Navigator>;
+}
+
+export default MainTab;

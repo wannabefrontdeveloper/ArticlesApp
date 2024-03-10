7b0bfb29a80e15b3f2a53b65fc388f5a8a10ef68
wannabefrontdeveloper <giwonk@naver.com>
2024-03-11 00:17:58 +0900

    코드생성 ArticleView 컴포넌트를 사용하도록 하였습니다.

--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -1,10 +1,12 @@
 import {RouteProp, useRoute} from '@react-navigation/core';
 import React from 'react';
-import {View, StyleSheet, Text, ActivityIndicator} from 'react-native';
+import {StyleSheet, ActivityIndicator, FlatList} from 'react-native';
 import {useQuery} from 'react-query';
 import {getArticle} from '../api/articles';
 import {getComments} from '../api/comments';
 import {RootStackParamList} from './types';
+import ArticleView from '../components/ArticleView';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 type ArticleScreenRouteProp = RouteProp<RootStackParamList, 'Article'>;
 
@@ -15,6 +17,8 @@ function ArticleScreen() {
   const articleQuery = useQuery(['article', id], () => getArticle(id));
   const commentsQuery = useQuery(['comments', id], () => getComments(id));
 
+  const {bottom} = useSafeAreaInsets();
+
   // 둘 중 하나라도 준비되지 않은 데이터가 있으면 스피너 보여주기
   if (!articleQuery.data || !commentsQuery.data) {
     return (
@@ -22,18 +26,37 @@ function ArticleScreen() {
     );
   }
 
+  const {title, body, published_at, user} = articleQuery.data;
+
   return (
-    <View>
-      <Text>{articleQuery.data.title}</Text>
-      <Text>{articleQuery.data.body}</Text>
-      <Text>{commentsQuery.data.length}개의 댓글</Text>
-    </View>
+    <FlatList
+      style={styles.flatList}
+      contentContainerStyle={[styles.flatListContent, {paddingBottom: bottom}]}
+      data={commentsQuery.data}
+      renderItem={() => null}
+      keyExtractor={item => item.id.toString()}
+      ListHeaderComponent={
+        <ArticleView
+          title={title}
+          body={body}
+          publishedAt={published_at}
+          username={user.username}
+        />
+      }
+    />
   );
 }
 
 const styles = StyleSheet.create({
   spinner: {
     flex: 1,
+  },
+  flatList: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  flatListContent: {
+    paddingHorizontal: 12,
   },
 });
 

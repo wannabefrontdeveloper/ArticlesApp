2772d91a338f59615bf044431c63e37c9332da4b
wannabefrontdeveloper <giwonk@naver.com>
2024-03-10 23:59:26 +0900

    코드수정 useQuery를 통해 선언했던 함수들을 사용하도록 하였습니다.

--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -1,21 +1,40 @@
 import {RouteProp, useRoute} from '@react-navigation/core';
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, ActivityIndicator} from 'react-native';
+import {useQuery} from 'react-query';
+import {getArticle} from '../api/articles';
+import {getComments} from '../api/comments';
 import {RootStackParamList} from './types';
 
 type ArticleScreenRouteProp = RouteProp<RootStackParamList, 'Article'>;
 
 function ArticleScreen() {
   const {params} = useRoute<ArticleScreenRouteProp>();
+  const {id} = params;
+
+  const articleQuery = useQuery(['article', id], () => getArticle(id));
+  const commentsQuery = useQuery(['comments', id], () => getComments(id));
+
+  // 둘 중 하나라도 준비되지 않은 데이터가 있으면 스피너 보여주기
+  if (!articleQuery.data || !commentsQuery.data) {
+    return (
+      <ActivityIndicator size="large" style={styles.spinner} color="black" />
+    );
+  }
+
   return (
-    <View style={styles.block}>
-      <Text>{params.id}</Text>
+    <View>
+      <Text>{articleQuery.data.title}</Text>
+      <Text>{articleQuery.data.body}</Text>
+      <Text>{articleQuery.data.length}개의 댓글</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  block: {},
+  spinner: {
+    flex: 1,
+  },
 });
 
 export default ArticleScreen;

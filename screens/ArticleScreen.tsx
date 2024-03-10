e4ffe1c0b47a9b6827a631eba3e357810a5f1735
wannabefrontdeveloper <giwonk@naver.com>
2024-03-10 23:35:05 +0900

    코드생성 게시글 정보를 보여주는 화면입니다.

---
+++ screens/ArticleScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+function ArticleScreen() {
+  return <View style={styles.block} />;
+}
+
+const styles = StyleSheet.create({
+  block: {},
+});
+
+export default ArticleScreen;

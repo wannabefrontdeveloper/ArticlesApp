a87bd60138ddff5f501b19674c8a8f0e512c1d0b
wannabefrontdeveloper <giwonk@naver.com>
2024-03-10 21:36:50 +0900

    코드수정 리액트 네비게이션과 관련한 초기설정을 완료하였습니다.

--- screens/MainTab.tsx
+++ screens/MainTab.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {MainTabParamList} from './types';
+import ArticlesScreen from './ArticlesScreen';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator<MainTabParamList>();
 
 function MainTab() {
-  return <Tab.Navigator>{/* TODO: 화면 추가 */}</Tab.Navigator>;
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Articles"
+        component={ArticlesScreen}
+        options={{
+          title: '게시글 목록',
+          tabBarIcon: ({color, size}) => (
+            <MaterialIcons name="article" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
 
 export default MainTab;
--- screens/RootStack.tsx
+++ screens/RootStack.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {RootStackParamList} from './types';
+import MainTab from './MainTab';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function RootStack() {
-  return <Stack.Navigator>{/* TODO: 화면 추가 */}</Stack.Navigator>;
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="MainTab"
+        component={MainTab}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
 }
 
 export default RootStack;

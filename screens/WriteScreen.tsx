22d97ac5c661e539e242ed0dc25d3bb49bfdff2a
wannabefrontdeveloper <giwonk@naver.com>
2024-03-12 17:55:23 +0900

    코드수정 헤더 우측에 작성 버튼을 만들어주었습니다.

--- screens/WriteScreen.tsx
+++ screens/WriteScreen.tsx
@@ -1,16 +1,39 @@
-import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import React, {useCallback, useEffect, useState} from 'react';
 import {
   KeyboardAvoidingView,
   Platform,
+  Pressable,
   StyleSheet,
   TextInput,
 } from 'react-native';
 import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
+import {RootStackNavigationProp} from './types';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 function WriteScreen() {
   const {top} = useSafeAreaInsets();
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
+
+  const navigation = useNavigation<RootStackNavigationProp>();
+  const onSubmit = useCallback(() => {
+    // TODO: 구현 예정
+  }, []);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRightContainerStyle: styles.headerRightContainer,
+      headerRight: () => (
+        <Pressable
+          hitSlop={8}
+          onPress={onSubmit}
+          style={({pressed}) => pressed && styles.headerRightPressed}>
+          <MaterialIcons name="send" color="#2196f3" size={24} />
+        </Pressable>
+      ),
+    });
+  }, [onSubmit, navigation]);
 
   return (
     <SafeAreaView style={styles.block} edges={['bottom']}>
@@ -60,6 +83,12 @@ const styles = StyleSheet.create({
     paddingBottom: 12,
     marginTop: 16,
     flex: 1,
+  },
+  headerRightContainer: {
+    marginRight: 16,
+  },
+  headerRightPressed: {
+    opacity: 0.75,
   },
 });
 

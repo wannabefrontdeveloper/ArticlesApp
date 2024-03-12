4f287bf41805e7740e977a1ae1bfcb0307d0a4ad
wannabefrontdeveloper <giwonk@naver.com>
2024-03-12 17:58:55 +0900

    코드수정 mutate 함수를 write라는 이름으로 변경하여 구조분해해주고, onSuccess를 통해 게시글 작성에 성공하면 게시글 작성 화면을 닫도록 구현하였습니다.

--- screens/WriteScreen.tsx
+++ screens/WriteScreen.tsx
@@ -10,16 +10,23 @@ import {
 import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
 import {RootStackNavigationProp} from './types';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {useMutation} from 'react-query';
+import {writeArticle} from '../api/articles';
 
 function WriteScreen() {
   const {top} = useSafeAreaInsets();
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
+  const {mutate: write} = useMutation(writeArticle, {
+    onSuccess: () => {
+      navigation.goBack();
+    },
+  });
 
   const navigation = useNavigation<RootStackNavigationProp>();
   const onSubmit = useCallback(() => {
-    // TODO: 구현 예정
-  }, []);
+    write({title, body});
+  }, [write, title, body]);
 
   useEffect(() => {
     navigation.setOptions({

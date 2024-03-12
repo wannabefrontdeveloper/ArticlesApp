539f3afb0d298e9361e201342dda9c56eecd2425
wannabefrontdeveloper <giwonk@naver.com>
2024-03-12 13:10:22 +0900

    코드수정 회원가입 또는 로그인을 하고 나면 사용자 메뉴 화면에서 로그아웃 버튼이 생기고, 로그아웃 기능도 구현하였습니다.

--- screens/UserMenuScreen.tsx
+++ screens/UserMenuScreen.tsx
@@ -3,17 +3,31 @@ import React from 'react';
 import {View} from 'react-native';
 import MenuItem from '../components/MenuItem';
 import {RootStackNavigationProp} from './types';
+import {clearToken} from '../api/client';
+import {useUserState} from '../contexts/UserContext';
 
 function UserMenuScreen() {
   const navigation = useNavigation<RootStackNavigationProp>();
 
+  const [user, setUser] = useUserState();
+
   const onLogin = () => navigation.navigate('Login');
   const onRegister = () => navigation.navigate('Register');
+  const onLogout = () => {
+    setUser(null);
+    clearToken();
+  };
 
   return (
     <View>
-      <MenuItem name="로그인" onPress={onLogin} />
-      <MenuItem name="회원가입" onPress={onRegister} />
+      {user ? (
+        <MenuItem name="로그아웃" onPress={onLogout} />
+      ) : (
+        <>
+          <MenuItem name="로그인" onPress={onLogin} />
+          <MenuItem name="회원가입" onPress={onRegister} />
+        </>
+      )}
     </View>
   );
 }

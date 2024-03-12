286a6c9a0b22d58fba1cce6bd7004525a19ea947
wannabefrontdeveloper <giwonk@naver.com>
2024-03-12 17:38:01 +0900

    코드수정 WriteScreen 컴포넌트를 등록해주었습니다.

--- screens/RootStack.tsx
+++ screens/RootStack.tsx
@@ -7,6 +7,7 @@ import RegisterScreen from './RegisterScreen';
 import LoginScreen from './LoginScreen';
 import MyArticlesScreen from './MyArticlesScreen';
 import useAuthLoadEffect from '../hooks/useAuthLoadEffect';
+import WriteScreen from './WriteScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -38,6 +39,11 @@ function RootStack() {
         name="Article"
         component={ArticleScreen}
         options={{title: '게시글'}}
+      />
+      <Stack.Screen
+        name="Write"
+        component={WriteScreen}
+        options={{title: '새 게시글 작성'}}
       />
     </Stack.Navigator>
   );

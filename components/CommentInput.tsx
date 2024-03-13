079e1b25f54a3e893bb4289e951834b9694bc18e
wannabefrontdeveloper <giwonk@naver.com>
2024-03-13 22:33:05 +0900

    코드수정 CommentModal을 불러와 사용하도록 하였습니다.

--- components/CommentInput.tsx
+++ components/CommentInput.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, Pressable} from 'react-native';
+import CommentModal from './CommentModal';
 
 export interface CommentInputProps {
   articleId: number;
 }
 
 function CommentInput({}: CommentInputProps) {
+  const [writingComment, setWritingComment] = useState(false);
+
   const onPress = () => {
+    setWritingComment(true);
+  };
+
+  const onClose = () => {
+    setWritingComment(false);
+  };
+
+  const onSubmit = (message: string) => {
+    setWritingComment(false);
     // TODO: 구현 예정
+    console.log(message);
   };
 
   return (
-    <Pressable style={styles.block} onPress={onPress}>
-      <Text style={styles.text}>댓글을 입력하세요</Text>
-    </Pressable>
+    <>
+      <Pressable style={styles.block} onPress={onPress}>
+        <Text style={styles.text}>댓글을 입력하세요</Text>
+      </Pressable>
+      <CommentModal
+        onClose={onClose}
+        visible={writingComment}
+        onSubmit={onSubmit}
+      />
+    </>
   );
 }
 

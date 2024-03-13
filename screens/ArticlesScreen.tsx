e0ac2ea26896a8148fb512a9666bcae6404a0cd1
wannabefrontdeveloper <giwonk@naver.com>
2024-03-13 16:02:42 +0900

    코드수정 useInfiniteQuery Hook을 사용하도록 하였습니다.

--- screens/ArticlesScreen.tsx
+++ screens/ArticlesScreen.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {ActivityIndicator, StyleSheet} from 'react-native';
-import {useQuery} from 'react-query';
+import {useInfiniteQuery} from 'react-query';
+import {Article} from '../api/types';
 import {getArticles} from '../api/articles';
 import Articles from '../components/Articles';
 import {useUserState} from '../contexts/UserContext';
 
 function ArticlesScreen() {
-  const {data} = useQuery('articles', getArticles);
+  const {data} = useInfiniteQuery(
+    'articles',
+    ({pageParam}) => getArticles({cursor: pageParam}),
+    {
+      getNextPageParam: lastPage =>
+        lastPage.length === 10 ? lastPage[lastPage.length - 1].id : undefined,
+    },
+  );
+
+  const items = useMemo(() => {
+    if (!data) {
+      return null;
+    }
+    return ([] as Article[]).concat(...data.pages);
+  }, [data]);
   const [user] = useUserState();
 
-  if (!data) {
-    return <ActivityIndicator size="large" style={styles.spinner} />;
+  if (!items) {
+    return (
+      <ActivityIndicator size="large" style={styles.spinner} color="black" />
+    );
   }
 
-  return <Articles articles={data} showWriteButton={!!user} />;
+  return <Articles articles={items} showWriteButton={!!user} />;
 }
 
 const styles = StyleSheet.create({

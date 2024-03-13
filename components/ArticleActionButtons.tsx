e1f021527395c0373b61a782b7c89576ba3ba61e
wannabefrontdeveloper <giwonk@naver.com>
2024-03-13 18:35:39 +0900

    코드수정 AskDialog 컴포넌트를 사용하도록 하였습니다.

--- components/ArticleActionButtons.tsx
+++ components/ArticleActionButtons.tsx
@@ -1,34 +1,53 @@
 import {useNavigation} from '@react-navigation/core';
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet, Pressable, Text} from 'react-native';
 import {RootStackNavigationProp} from '../screens/types';
+import AskDialog from './AskDialog';
 
 export interface ArticleActionButtonsProps {
   articleId: number;
 }
 
 function ArticleActionButtons({articleId}: ArticleActionButtonsProps) {
+  const [askRemove, setAskRemove] = useState(false);
   const navigation = useNavigation<RootStackNavigationProp>();
   const onPressModify = () => {
     navigation.navigate('Write', {articleId});
   };
   const onPressRemove = () => {
-    // TODO: 구현 예정
+    setAskRemove(true);
+  };
+  const onCancelRemove = () => {
+    setAskRemove(true);
+  };
+  const onConfirmRemove = () => {
+    setAskRemove(true);
   };
   return (
-    <View style={styles.block}>
-      <Pressable
-        style={({pressed}) => pressed && styles.pressed}
-        onPress={onPressModify}>
-        <Text style={styles.buttonText}>수정</Text>
-      </Pressable>
-      <View style={styles.separator} />
-      <Pressable
-        onPress={onPressRemove}
-        style={({pressed}) => pressed && styles.pressed}>
-        <Text style={styles.buttonText}>삭제</Text>
-      </Pressable>
-    </View>
+    <>
+      <View style={styles.block}>
+        <Pressable
+          style={({pressed}) => pressed && styles.pressed}
+          onPress={onPressModify}>
+          <Text style={styles.buttonText}>수정</Text>
+        </Pressable>
+        <View style={styles.separator} />
+        <Pressable
+          onPress={onPressRemove}
+          style={({pressed}) => pressed && styles.pressed}>
+          <Text style={styles.buttonText}>삭제</Text>
+        </Pressable>
+      </View>
+      <AskDialog
+        visible={askRemove}
+        title="게시글 삭제"
+        message="게시글을 삭제하시겠습니까?"
+        isDestructive
+        confirmText="삭제"
+        onConfirm={onConfirmRemove}
+        onClose={onCancelRemove}
+      />
+    </>
   );
 }
 

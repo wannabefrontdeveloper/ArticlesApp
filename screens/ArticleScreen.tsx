7c6296902a633e05a4ccd173bc57ea87add8d4e1
wannabefrontdeveloper <giwonk@naver.com>
2024-03-13 17:05:12 +0900

    코드수정 현재 사용자 정보와 게시글 정보를 비교하고 ArticleActionButtons에 articleId를 설정할 수 있도록 id 값도 Props로 넣어주었습니다.

--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -8,12 +8,14 @@ import {RootStackParamList} from './types';
 import ArticleView from '../components/ArticleView';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import CommentItem from '../components/CommentItem';
+import {useUserState} from '../contexts/UserContext';
 
 type ArticleScreenRouteProp = RouteProp<RootStackParamList, 'Article'>;
 
 function ArticleScreen() {
   const {params} = useRoute<ArticleScreenRouteProp>();
   const {id} = params;
+  const [currentUser] = useUserState();
 
   const articleQuery = useQuery(['article', id], () => getArticle(id));
   const commentsQuery = useQuery(['comments', id], () => getComments(id));
@@ -28,6 +30,7 @@ function ArticleScreen() {
   }
 
   const {title, body, published_at, user} = articleQuery.data;
+  const isMyArticle = currentUser?.id === user.id;
 
   return (
     <FlatList
@@ -49,6 +52,8 @@ function ArticleScreen() {
           body={body}
           publishedAt={published_at}
           username={user.username}
+          id={id}
+          isMyArticle={isMyArticle}
         />
       }
     />
